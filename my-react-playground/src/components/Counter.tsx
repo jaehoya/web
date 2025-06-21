dae8c977b5a8f01dd4d3597c2720fee5f03e2a71
jaehoya <hmujaeho@gmail.com>
2025-06-22 04:10:55 +0900

    Counter 만들었음

---
+++ my-react-playground/src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
