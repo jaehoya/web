6ac2a46db69373013acec2929af889750b83f15f
jaehoya <hmujaeho@gmail.com>
2025-06-24 19:15:17 +0900

    6/24 파일 날아가는 거 무서워서 커밋

--- my-react-playground/src/components/common/header/CommonHeader.tsx
+++ my-react-playground/src/components/common/header/CommonHeader.tsx
@@ -1,9 +1,19 @@
 import styles from './CommonHeader.module.scss'
 
 function CommonHeader() {
-  return (
-    <div>CommonHeader</div>
-  )
+	return (
+		<div className={styles.header}>
+			<div className={styles.header_logoox}>
+				<img src="" alt="" />
+				<span className={styles.header_logoBox__title}>PhotoSplash</span>
+			</div>
+			<div className={styles.header__profileBox}>
+				<button className={styles.header__profileBox__button}>사진 제출</button>
+				<button className={styles.header__profileBox__button}>북마크</button>
+				<span className={styles.header__profileBox__userName}>이재호 ㅣ@jaeho_instagram</span>
+			</div>
+		</div>
+	)
 }
 
 export default CommonHeader
