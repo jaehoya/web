e5e3617fc5a57bb4ac01b1122417d8b79b2ae653
jaehoya <hmujaeho@gmail.com>
2025-07-01 16:08:25 +0900

    [ develope-zustand 04 ] 페이지 변경하는 것과 검색 기능 구현

--- my-react-playground/src/components/common/footer/CommonFooter.tsx
+++ my-react-playground/src/components/common/footer/CommonFooter.tsx
@@ -1,16 +1,21 @@
 import styles from './CommonFooter.module.scss';
+import { useImageStore } from '../../../store/useImagegStore';
+
 
 function CommonFooter() {
+   const setPage = useImageStore(state => state.setPage);
+   const page = useImageStore(state => state.page);
+
    return (
       <footer className={styles.footer}>
          <div className={styles.pagination}>
             <button className={styles.pagination__button}>
-               <img src="src\assets\icons\icon-arrowLeft.svg" alt=" " />
+               <img src="src\assets\icons\icon-arrowLeft.svg" alt=" " onClick={() => setPage(page - 1)} />
             </button>
-            {/* 변결될 UI 부분 */}
-            <span>1</span>
+            {/* 변경될 UI 부분 */}
+            <span>{page}</span>
             <button className={styles.pagination__button}>
-               <img src="src\assets\icons\icon-arrowRight.svg" alt=" " />
+               <img src="src\assets\icons\icon-arrowRight.svg" alt=" " onClick={() => setPage(page + 1)} />
             </button>
          </div>
       </footer>
--- my-react-playground/src/components/common/searchBar/CommonSerachBar.tsx
+++ my-react-playground/src/components/common/searchBar/CommonSerachBar.tsx
@@ -1,14 +1,38 @@
 import styles from './CommonSerachBar.module.scss';
+import { useImageStore } from '../../../store/useImagegStore';
+import { useState } from 'react';
 
 function CommonSerachBar() {
+   const [value, setValue] = useState('');
+   const setSearchValue = useImageStore(state => state.setSearchValue);
+
+   const handleSearch = () => {
+      if (!value.trim()) return;
+      setSearchValue(value);
+      setValue('');
+   };
+
+   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+         handleSearch();
+      }
+   };
+
    return (
       <div className={styles.searchBar}>
          <div className={styles.searchBar__search}>
-            <input type="text" placeholder='찾으실 이미지를 검색하세요' className={styles.searchBar__search__input}/>
-            <img src="src/assets/icons/icon-search.svg" alt="" />
+            <input
+               type="text"
+               placeholder='찾으실 이미지를 검색하세요'
+               className={styles.searchBar__search__input}
+               value={value}
+               onChange={(e) => setValue(e.target.value)}
+               onKeyDown={handleKeyDown}
+            />
+            <img src="src/assets/icons/icon-search.svg" alt="" onClick={handleSearch} />
          </div>
       </div>
-   )
+   );
 }
 
 export default CommonSerachBar
